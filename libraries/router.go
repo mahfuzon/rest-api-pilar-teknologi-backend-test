a1bb2bc1593f4cdc1207373e6320954d897f7d2c
mahfuzon <mahfuzon0@gmail.com>
2023-05-11 17:16:33 +0700

    function setup echo router

---
+++ libraries/router.go
@@ -0,0 +1,12 @@
+package libraries
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+func SetRouter() *echo.Echo {
+	e := echo.New()
+	e.Validator = &CustomValidator{Validator: validator.New()}
+	return e
+}
