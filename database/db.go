780a00323e05ee41b4bf1c3c0783e02f97463710
mahfuzon <mahfuzon0@gmail.com>
2023-05-11 17:11:21 +0700

    buat fungsi setDb dan setDbtest

---
+++ database/db.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func SetDb() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/db_pilar?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return db
+}
+
+func SetDbTest() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/db_pilar_test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return db
+}
