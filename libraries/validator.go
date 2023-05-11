5e08cac33aae10afd8a62879c9f865104777e3a2
mahfuzon <mahfuzon0@gmail.com>
2023-05-11 17:10:37 +0700

    buat custom validator yang sesuai dengan contruct echo.Validate

---
+++ libraries/validator.go
@@ -0,0 +1,18 @@
+package libraries
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+type CustomValidator struct {
+	Validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	err := cv.Validator.Struct(i)
+	if err != nil {
+		// Optionally, you could return the error to give each route more control over the status code
+		return err
+	}
+	return nil
+}
