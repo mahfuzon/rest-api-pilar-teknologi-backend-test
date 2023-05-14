cd6f5cb967653daabd616d94a795a6d1bb4616ee
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2023-05-14 22:04:20 +0700

    tambahin function logger di set db

--- database/db.go
+++ database/db.go
@@ -3,13 +3,15 @@ package database
 import (
 	"log"
 
+	"gorm.io/gorm/logger"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func SetDb() *gorm.DB {
 	dsn := "root:@tcp(127.0.0.1:3306)/db_pilar?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -19,7 +21,7 @@ func SetDb() *gorm.DB {
 
 func SetDbTest() *gorm.DB {
 	dsn := "root:@tcp(127.0.0.1:3306)/db_pilar_test?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
