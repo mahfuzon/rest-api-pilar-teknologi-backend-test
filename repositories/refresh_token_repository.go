28c59c1350d01a7b82b5b0df75f85e03858f3e7c
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2023-05-15 14:48:29 +0700

    done refresh token repository

---
+++ repositories/refresh_token_repository.go
@@ -0,0 +1,41 @@
+package repositories
+
+import (
+	"github.com/pilar_test_rest_api/models"
+	"gorm.io/gorm"
+)
+
+type RefreshTokenRepository interface {
+	Create(refreshToken *models.RefreshToken) error
+	Find(userId int, refreshToken string) (models.RefreshToken, error)
+}
+
+type refreshTokenRepository struct {
+	db *gorm.DB
+}
+
+func NewRefreshTokenRepository(db *gorm.DB) RefreshTokenRepository {
+	return &refreshTokenRepository{
+		db: db,
+	}
+}
+
+func (refreshTokenRepository *refreshTokenRepository) Create(refreshToken *models.RefreshToken) error {
+	err := refreshTokenRepository.db.Create(refreshToken).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (refreshTokenRepository *refreshTokenRepository) Find(userId int, refreshToken string) (models.RefreshToken, error) {
+	refreshTokenModel := models.RefreshToken{}
+	err := refreshTokenRepository.db.Where("user_id = ?", userId).Where("token = ?", refreshToken).First(&refreshTokenModel).Error
+	if err != nil {
+		return refreshTokenModel, err
+	}
+
+	return refreshTokenModel, nil
+
+}
