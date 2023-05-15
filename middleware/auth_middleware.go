56f125c81156fe2cf3c9412542095403dd093b3e
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2023-05-15 14:47:50 +0700

    refactor auth middleware

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,12 +1,13 @@
 package middleware
 
 import (
-	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
+	"github.com/pilar_test_rest_api/libraries"
 	"github.com/pilar_test_rest_api/response"
 	"github.com/pilar_test_rest_api/services"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func AuthMiddleware(userService services.UserService, authService services.AuthService) echo.MiddlewareFunc {
@@ -15,12 +16,12 @@ func AuthMiddleware(userService services.UserService, authService services.AuthS
 			authHeader := c.Request().Header.Get("Authorization")
 
 			if !strings.Contains(authHeader, "Bearer") {
-				response := response.APIResponse{
+				apiResponse := response.APIResponse{
 					Status:  "error",
 					Message: "Unauthorized",
 					Data:    nil,
 				}
-				return c.JSON(http.StatusUnauthorized, response)
+				return c.JSON(http.StatusUnauthorized, apiResponse)
 			}
 
 			tokenString := ""
@@ -29,37 +30,45 @@ func AuthMiddleware(userService services.UserService, authService services.AuthS
 				tokenString = arrayToken[1]
 			}
 
-			token, err := authService.ValidateToken(tokenString)
+			token, err := authService.ValidateToken(tokenString, "access")
 			if err != nil {
-				response := response.APIResponse{
+				apiResponse := response.APIResponse{
 					Status:  "error",
 					Message: "Unauthorized",
 					Data:    err.Error(),
 				}
-				return c.JSON(http.StatusUnauthorized, response)
+				return c.JSON(http.StatusUnauthorized, apiResponse)
 			}
 
-			claim, ok := token.Claims.(jwt.MapClaims)
-
-			if !ok || !token.Valid {
-				response := response.APIResponse{
+			claim, err := libraries.DecodeEncodedTokenToMapClaim(token)
+			if err != nil {
+				apiResponse := response.APIResponse{
 					Status:  "error",
 					Message: "Unauthorized",
 					Data:    err.Error(),
 				}
-				return c.JSON(http.StatusUnauthorized, response)
+				return c.JSON(http.StatusUnauthorized, apiResponse)
 			}
 
-			userID := int(claim["user_id"].(float64))
+			if time.Now().Unix() > claim.ExpiredAt {
+				apiResponse := response.APIResponse{
+					Status:  "error",
+					Message: "Unauthorized",
+					Data:    "token expired",
+				}
+				return c.JSON(http.StatusUnauthorized, apiResponse)
+			}
+
+			userID := claim.UserId
 
 			userResponse, err := userService.FindById(userID)
 			if err != nil {
-				response := response.APIResponse{
+				apiResponse := response.APIResponse{
 					Status:  "error",
 					Message: "Unauthorized",
 					Data:    err.Error(),
 				}
-				return c.JSON(http.StatusUnauthorized, response)
+				return c.JSON(http.StatusUnauthorized, apiResponse)
 			}
 
 			c.Set("user", userResponse)
