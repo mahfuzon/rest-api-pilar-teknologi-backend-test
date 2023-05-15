b17693cf18be44bfb8b43beb7de41453da3ef695
Mahfuzon Akhiar <mahfuzon0@gmail.com>
2023-05-15 14:49:46 +0700

    refactor auth service and user service

---
+++ services/auth_service.go
@@ -0,0 +1,116 @@
+package services
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt"
+	"github.com/pilar_test_rest_api/libraries"
+	"github.com/pilar_test_rest_api/models"
+	"github.com/pilar_test_rest_api/repositories"
+	"github.com/pilar_test_rest_api/request"
+	"github.com/pilar_test_rest_api/response"
+	"os"
+	"time"
+)
+
+type AuthService interface {
+	GenerateAccessToken(userID int) (string, error)
+	ValidateToken(token string, typeToken string) (*jwt.Token, error)
+	GenerateNewAccessToken(tokenRequest request.CreateNewAccessTokenRequest) (response.CreateNewAccessTokenResponse, error)
+	FindRefreshToken(userId int, findRefreshTokenRequest request.FindRefreshTokenRequest) (models.RefreshToken, error)
+}
+
+type authService struct {
+	refreshTokenRepository repositories.RefreshTokenRepository
+}
+
+func NewAuthService(refreshTokenRepository repositories.RefreshTokenRepository) AuthService {
+	return &authService{
+		refreshTokenRepository: refreshTokenRepository,
+	}
+}
+
+func (s *authService) FindRefreshToken(userId int, findRefreshTokenRequest request.FindRefreshTokenRequest) (models.RefreshToken, error) {
+	refreshToken, err := s.refreshTokenRepository.Find(userId, findRefreshTokenRequest.RefreshToken)
+	if err != nil {
+		return refreshToken, err
+	}
+
+	return refreshToken, nil
+}
+
+func (s *authService) GenerateAccessToken(userID int) (string, error) {
+	token, err := libraries.GenerateNewToken(userID, "access")
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
+}
+
+func (s *authService) ValidateToken(encodedToken string, typeToken string) (*jwt.Token, error) {
+	secretKey := os.Getenv("jwt_secret_key")
+	if typeToken == "refresh" {
+		secretKey = os.Getenv("jwt_secret_refresh_token_key")
+	}
+	token, err := libraries.VerifyTokenBySecretKey(encodedToken, secretKey)
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
+}
+
+func (s *authService) GenerateNewAccessToken(request request.CreateNewAccessTokenRequest) (response.CreateNewAccessTokenResponse, error) {
+	createNewAccessTokenResponse := response.CreateNewAccessTokenResponse{}
+
+	oldRefreshToken, err := s.refreshTokenRepository.Find(request.UserId, request.RefreshToken)
+	if err != nil {
+		return createNewAccessTokenResponse, err
+	}
+
+	// verify token base on secret key
+	tokenJwt, err := libraries.VerifyTokenBySecretKey(oldRefreshToken.Token, os.Getenv("jwt_secret_refresh_token_key"))
+	if err != nil {
+		return createNewAccessTokenResponse, err
+	}
+
+	claim, err := libraries.DecodeEncodedTokenToMapClaim(tokenJwt)
+
+	//verify user id
+	if oldRefreshToken.UserId != claim.UserId {
+		return createNewAccessTokenResponse, errors.New("invalid user id")
+	}
+
+	//verify expired token
+	if time.Now().Unix() > claim.ExpiredAt {
+		return createNewAccessTokenResponse, errors.New("token expired")
+	}
+
+	newRefreshTokenEncode, err := libraries.GenerateNewToken(oldRefreshToken.UserId, "refresh")
+	if err != nil {
+		return createNewAccessTokenResponse, errors.New("failed generate refresh token")
+	}
+
+	newRefreshToken := models.RefreshToken{
+		Token:  newRefreshTokenEncode,
+		UserId: request.UserId,
+	}
+
+	err = s.refreshTokenRepository.Create(&newRefreshToken)
+	if err != nil {
+		return createNewAccessTokenResponse, errors.New("failed create new refresh token")
+	}
+
+	// generate new access token
+	newAccessTokenEncode, err := libraries.GenerateNewToken(request.UserId, "access")
+	if err != nil {
+		return createNewAccessTokenResponse, err
+	}
+	// end
+
+	createNewAccessTokenResponse.Token = newAccessTokenEncode
+
+	return createNewAccessTokenResponse, nil
+}
--- services/user_service.go
+++ services/user_service.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"errors"
+	"fmt"
+	"github.com/pilar_test_rest_api/libraries"
 	"github.com/pilar_test_rest_api/models"
 	"github.com/pilar_test_rest_api/repositories"
 	"github.com/pilar_test_rest_api/request"
@@ -9,31 +12,46 @@ import (
 )
 
 type UserService interface {
-	Register(request request.UserRegisterRequest) (response.UserResponse, error)
+	Register(request request.UserRegisterRequest) (response.UserRegisterResponse, error)
 	FindById(id int) (response.UserResponse, error)
-	Login(loginRequest request.UserLoginRequest) (response.UserResponse, error)
+	Login(loginRequest request.UserLoginRequest) (response.LoginResponse, error)
 	UploadProfileImage(url string, userId int) (response.UserResponse, error)
 }
 
 type userService struct {
 	userRepository repositories.UserRepository
+	repositories.RefreshTokenRepository
 }
 
-func NewUserService(userRepository repositories.UserRepository) UserService {
+func NewUserService(userRepository repositories.UserRepository, refreshTokenRepository repositories.RefreshTokenRepository) UserService {
 	return &userService{
-		userRepository: userRepository,
+		userRepository:         userRepository,
+		RefreshTokenRepository: refreshTokenRepository,
 	}
 }
 
-func (service *userService) Register(userRegisterRequest request.UserRegisterRequest) (response.UserResponse, error) {
+func (service *userService) Register(userRegisterRequest request.UserRegisterRequest) (response.UserRegisterResponse, error) {
 	user := models.User{}
-	userResponse := response.UserResponse{}
+	userRegisterResponse := response.UserRegisterResponse{}
 
+	// cek user is exists
+	checUserIsExist, err := service.userRepository.FindByEmail(userRegisterRequest.Email)
+	if err != nil {
+		fmt.Println(checUserIsExist)
+		if checUserIsExist.Id != 0 {
+			return userRegisterResponse, errors.New("user already exists")
+		}
+	}
+	// end
+
+	// hash password
 	passWordHash, err := bcrypt.GenerateFromPassword([]byte(userRegisterRequest.Password), bcrypt.MinCost)
 	if err != nil {
-		return userResponse, err
+		return userRegisterResponse, err
 	}
+	// end
 
+	// fill all field object user model
 	user.Email = userRegisterRequest.Email
 	user.Password = string(passWordHash)
 	user.FirstName = userRegisterRequest.FirstName
@@ -43,46 +61,124 @@ func (service *userService) Register(userRegisterRequest request.UserRegisterReq
 	user.Province = userRegisterRequest.Province
 	user.Country = userRegisterRequest.Country
 	user.Telephone = userRegisterRequest.Telephone
+	// end
 
+	// create new user
 	err = service.userRepository.Create(&user)
 	if err != nil {
-		return userResponse, err
+		return userRegisterResponse, err
 	}
+	// end
 
-	userResponse.ParseToUserResponse(user)
+	// generate access token and refresh token
+	accessTokenString, err := libraries.GenerateNewToken(user.Id, "access")
+	refreshTokenString, err := libraries.GenerateNewToken(user.Id, "refresh")
+	if err != nil {
+		return userRegisterResponse, err
+	}
+	// end
 
-	return userResponse, nil
+	// create refresh token
+	refreshToken := models.RefreshToken{
+		Token:  refreshTokenString,
+		UserId: user.Id,
+	}
+	err = service.RefreshTokenRepository.Create(&refreshToken)
+	if err != nil {
+		return userRegisterResponse, err
+	}
+	// end
+
+	// fill all field userResponse
+	userResponse := response.UserResponse{
+		Id:           user.Id,
+		Email:        user.Email,
+		FirstName:    user.FirstName,
+		LastName:     user.LastName,
+		Telephone:    user.Telephone,
+		ProfileImage: user.ProfileImage,
+		Address:      user.Address,
+		City:         user.City,
+		Province:     user.Province,
+		Country:      user.Country,
+	}
+	// end
+
+	// fill all user login response
+	userLoginResponse := response.LoginResponse{
+		Token:        accessTokenString,
+		RefreshToken: refreshTokenString,
+	}
+	// end
+
+	userRegisterResponse.User = userResponse
+	userRegisterResponse.Token = userLoginResponse
+
+	return userRegisterResponse, nil
 }
 
 func (service *userService) FindById(id int) (response.UserResponse, error) {
 	userResponse := response.UserResponse{}
 
+	// find user
 	user, err := service.userRepository.FindById(id)
 	if err != nil {
 		return userResponse, err
 	}
+	// end
 
 	userResponse.ParseToUserResponse(user)
 
 	return userResponse, nil
 }
 
-func (service *userService) Login(loginRequest request.UserLoginRequest) (response.UserResponse, error) {
-	userResponse := response.UserResponse{}
+func (service *userService) Login(loginRequest request.UserLoginRequest) (response.LoginResponse, error) {
+	// buat object kosong login response
+	loginResponse := response.LoginResponse{}
+	// end
 
+	// get user by email
 	user, err := service.userRepository.FindByEmail(loginRequest.Email)
 	if err != nil {
-		return userResponse, err
+		return loginResponse, err
 	}
+	// end
 
+	// validate password
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password))
 	if err != nil {
-		return userResponse, err
+		return loginResponse, err
+	}
+	// end
+
+	// generate access token string
+	accessTokenString, err := libraries.GenerateNewToken(user.Id, "access")
+	if err != nil {
+		return loginResponse, err
 	}
 
-	userResponse.ParseToUserResponse(user)
+	// generate refresh token string
+	refreshTokenString, err := libraries.GenerateNewToken(user.Id, "refresh")
+	if err != nil {
+		return loginResponse, err
+	}
+	// end
 
-	return userResponse, nil
+	// create refreshToken
+	refreshToken := models.RefreshToken{
+		Token:  refreshTokenString,
+		UserId: user.Id,
+	}
+	err = service.RefreshTokenRepository.Create(&refreshToken)
+	if err != nil {
+		return loginResponse, err
+	}
+	// end create refresh token
+
+	loginResponse.Token = accessTokenString
+	loginResponse.RefreshToken = refreshTokenString
+
+	return loginResponse, nil
 }
 
 func (service *userService) UploadProfileImage(url string, userId int) (response.UserResponse, error) {
